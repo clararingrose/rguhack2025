f48a596ca5fd2b73e71d67527621b7f988288094
clararingrose <2001824@rgu.ac.uk>
2025-02-22 16:39:19 +0000

    misc

--- assets/scripts/responsive.js
+++ assets/scripts/responsive.js
@@ -1,10 +1,10 @@
 // function to resize nav bar on smaller screens
 function resizeNavBar() {
-    var x = document.getElementById("navBar");
-    if (x.className === "nav") {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
       x.className += " responsive";
     } else {
-      x.className = "nav";
+      x.className = "topnav";
     }
   }
   
