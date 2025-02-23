dafddcf5c77e3f10215f0dc885e9a5b3a40bebd8
clararingrose <2001824@rgu.ac.uk>
2025-02-23 07:59:13 +0000

    test

--- public/assets/scripts/responsive.js
+++ public/assets/scripts/responsive.js
@@ -6,5 +6,11 @@ function resizeNavBar() {
     } else {
       x.className = "topnav";
     }
-  }
+}
   
+
+// Function to download data to a file
+var chatoutput = document.getElementById('conversation');
+var linksoutput = document.getElementById('download');
+download.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(VALUE));
+download.setAttribute('download', 'filename.csv');
